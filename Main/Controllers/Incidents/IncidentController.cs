4ae75ef0751440328d74e2b8c3a3d94da47fc800
AGranosik <adrian.granosik@gmail.com>
2023-09-30 12:41:59 +0200

    init

---
+++ Main/Controllers/Incidents/IncidentController.cs
@@ -0,0 +1,16 @@
+﻿using Main.Controllers.Incidents.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Main.Controllers.Incidents
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class IncidentController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> CreateIncidentAsync(IncidentCreationRequest request, CancellationToken cancellationToken)
+        {
+            return null;
+        }
+    }
+}
