e68061b4cfdad6aa8a2df895406787587be6efca
AGranosik <adrian.granosik@gmail.com>
2023-09-30 12:44:16 +0200

    init

---
+++ Main/Controllers/Incidents/Requests/IncidentCreationRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Main.Controllers.Incidents.Requests
+{
+    public class IncidentCreationRequest
+    {
+        public string Type { get; set; }
+        public string Image { get; set; }
+
+        //not sure how to store them yet.
+        public int Coordinates { get; set; }
+
+
+    }
+}
---
+++ Main/Program.cs
@@ -0,0 +1,28 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseCors(builder => builder
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .SetIsOriginAllowed((host) => true)
+        .AllowCredentials());
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
